258c65b5cd7f6239ae268bad642da31b5d12ef5b
Stroz_Dennis <stroz.dennis@gmail.com>
2023-10-19 14:15:39 +0200

    Prak1 Ulam Funktion

--- main.c
+++ main.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
-void addAndPrint(int a, int b){
-	int sum = a + b;
-	printf("Die Summe beträgt: %d \n", sum); //Option,Shift,7
-}
+void ulam(int a0);
 
 int main()
 {
-	addAndPrint(1, 7);
+	ulam(7);
+}
 
-	int num = 100;
-
-	while (num / 2 > 10){
-		num = num / 2;
+void ulam(int a0)
+{
+	if (a0 == 1)
+	{
+		printf("%d\n", 1);
 	}
-
-	printf("%d\n", num);
-
-	double x = 3.11;
-	int y = 5;
-
-	printf("%f\n", (float)y);
-
-	printf("%o", 42);
-
-	return 0;
+	else if (a0 % 2 == 1)
+	{
+		printf("%d\n", a0);
+		ulam(3 * a0 + 1);
+	}
+	else if (a0 % 2 == 0)
+	{
+		printf("%d\n", a0);
+		ulam(a0 / 2);
+	}
 }
