b095fd774e88e9fa7dc4b1af36fbd007ce4b32d1
Strozzii <stroz.dennis@gmail.com>
2023-10-28 15:29:24 +0200

    :sparkles: Berechnung einer n x n - Matrix mit n > 2

--- src/praktikum_2/ppr_prak2.c
+++ src/praktikum_2/ppr_prak2.c
@@ -1,13 +1,11 @@
-//
-// Created by de56008 on 26.10.2023.
-//
-
 #include "ppr_prak2.h"
 
 void print_matrix(double matrix[MAX_SIZE][MAX_SIZE], int size)
 {
+	/* Iteriere durch alle Zeilen der Matrix */
 	for (int i = 0; i < size; i++)
 	{
+		/* Iteriere durch alle Spalten der Matrix */
 		for (int j = 0; j < size; j++)
 		{
 			printf("%.1f ", matrix[i][j]);
@@ -17,16 +15,87 @@ void print_matrix(double matrix[MAX_SIZE][MAX_SIZE], int size)
 	printf("\n");
 }
 
+/**
+ * Diese Hilfsfunktion dient dazu Untermatrizen einer Matrix zu erzeugen und handhabt das Vorzeichen für den
+ * Laplaceschen Entwicklungssatz.
+ *
+ * @param matrix: n x n - Matrix zu der die Untermatrizen gebaut werden sollen.
+ * @param size: Größe n der n x n - Matrix.
+ * @param row: Zeile, an der sich die Entwicklung orientieren soll. Standardwert ist 0.
+ * @param col: Spalte, die bei der Entwicklung ignoriert werden soll. Dieser Wert ist variabel um die verschiedenen
+ * 			   Untermatrizen zu bauen und entscheidet das Vorzeichen.
+ *
+ * @return Determinante der erzeugten Untermatrizen durch indirekte Rekursion.
+ */
+double cofactor(double matrix[MAX_SIZE][MAX_SIZE], int size, int row, int col)
+{
+	double minor[MAX_SIZE][MAX_SIZE];
+
+	/* Laufvariablen für die eingehende Matrix */
+	int i;
+	int j;
+
+	/* Laufvariablen der Untermatrix */
+	int minorRow = 0;
+	int minorCol = 0;
+
+	/*
+	 * Durchlaufe sämtliche Werte der eingehenden Matrix und überspringe eine Iteration, wenn die Indizes den Werten
+	 * von row und col entsprechen, um nur auf die entsprechenden Werte für die Untermatrix zu zeigen, die in Dieser
+	 * auch gespeichert werden.
+	 */
+	for (i = 0; i < size; i++)
+	{
+		if (i == row)
+			continue;
+
+		for (j = 0; j < size; j++)
+		{
+			if (j == col)
+				continue;
+
+			minor[minorRow][minorCol] = matrix[i][j];
+			minorCol++;
+		}
+
+		minorRow++;
+		minorCol = 0;
+	}
+
+	return col % 2 == 0 ? get_determinant(minor, size - 1)
+						: -get_determinant(minor, size - 1);
+}
+
 double get_determinant(double matrix[MAX_SIZE][MAX_SIZE], int size)
 {
+	/*
+	 * Die Determinante von einer n x n - Matrix kann sehr simpel berechnet werden, wenn n < 3.
+	 * Die komplexe Berechnung findet statt, wenn n > 2 && n < 11.
+	 * Nicht berücksichtigte Angaben werfen eine Fehlermeldung in der Konsole aus und geben 0.0 zurück.
+	 */
 	if (size == 1)
 	{
 		return matrix[0][0];
 	}
 	else if (size == 2)
 	{
-		return (matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]);
+		return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
 	}
-	return -1;
+	else if (size > 2 && size < 11)
+	{
+		double det = 0;  /* Startwert der Determinante */
 
+		/* Iteriere entlang der ersten Zeile gemäß dem Entwicklungssatz */
+		for (int j = 0; j < size; j++)
+		{
+			/* Summenbildung mit Aufruf der Hilfsfunktion, die die Untermatrizen mit Vorzeichen liefert */
+			det += matrix[0][j] * cofactor(matrix, size, 0, j);
+		}
+		return det;
+	}
+	else
+	{
+		printf("FEHLER!");
+		return 0.0;
+	}
 }
--- src/praktikum_2/ppr_prak2.h
+++ src/praktikum_2/ppr_prak2.h
@@ -1,17 +1,35 @@
-//
-// Created by de56008 on 26.10.2023.
-//
+/**
+ * @file
+ * In diesem Modul werden die Aufgaben zu Matrizen und Determinanten umgesetzt. Dieses Blatt umfasst zwei Aufgaben.
+ *
+ * @author Dennis Stroz
+ * @date 2023-10-28
+ */
+#ifndef PROZEDURALE_PROGRAMMIERUNG_PPR_PRAK2_H
+#define PROZEDURALE_PROGRAMMIERUNG_PPR_PRAK2_H
+#define MAX_SIZE 10  /* Maximale Größe der quadratischen Matrizen */
 
 #include <stdio.h>
 
-#ifndef PROZEDURALE_PROGRAMMIERUNG_PPR_PRAK2_H
-#define PROZEDURALE_PROGRAMMIERUNG_PPR_PRAK2_H
-#define MAX_SIZE 10
-
 #endif //PROZEDURALE_PROGRAMMIERUNG_PPR_PRAK2_H
 
+/**
+ * Diese Funktion gibt in der Konsole die Werte einer n x n - Matrix aus.
+ *
+ * @param matrix: Auszugebene Matrix.
+ * @param size: Größe n der n x n - Matrix.
+ */
 void print_matrix(double matrix[MAX_SIZE][MAX_SIZE], int size);
 
+/**
+ * Diese Funktion berechnet die Determinante einer n x n - Matrix mithilfe des Laplaceschen Entwicklungssatz.
+ *
+ * @param matrix: n x n - Matrix zu der die Determinante berechnet werden soll.
+ * @param size: Größe n der n x n - Matrix.
+ *
+ * @return Gibt die Determinante zurück. Bei unsinnigen Eingaben wird 0.0 zurückgegeben und es wird eine Fehlermeldung
+ * in der Konsole ausgegeben.
+ */
 double get_determinant(double matrix[MAX_SIZE][MAX_SIZE], int size);
 
 
